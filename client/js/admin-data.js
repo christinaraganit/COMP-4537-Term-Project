ec697ba73785807afa9b2daea5b3ad8e33a31d1d
Christina Raganit <craganit@my.bcit.ca>
2021-04-09 18:31:00 -0700

    admin-data html and js added

---
+++ client/js/admin-data.js
@@ -0,0 +1,298 @@
+const endPointRoot = "https://ashergum.com/Comp4537/termproject/API/V1";
+const xhttp = new XMLHttpRequest();
+
+// ---------------------------------------BAKERIES--------------------------------------------
+function getBakeries() {
+  (async () => {
+    let result = await fetch(endPointRoot + "/bakery")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((res) => {
+        document.getElementById("container").innerHTML = "";
+
+        let bakeryTitle = document.createElement("h2");
+        bakeryTitle.setAttribute("class", "text-primary font-weight-bold");
+        bakeryTitle.innerHTML = "Bakeries";
+
+        let button = document.createElement("BUTTON");
+        button.innerHTML = "Add new bakery";
+        button.setAttribute("class", "w-100 btn btn-outline-primary");
+        button.setAttribute("onclick", "addBakeryFields()");
+        button.setAttribute("style", "margin-bottom: 24px");
+        button.setAttribute("data-toggle", "modal");
+        button.setAttribute("data-target", "#addBakeryModal");
+
+        document.getElementById("container").appendChild(bakeryTitle);
+        document.getElementById("container").appendChild(button);
+        for (let i = 0; i < res.length; i++) {
+          generateBakery(res[i]);
+        }
+      });
+  })();
+}
+
+function addBakeryFields() {}
+
+function addBakery() {}
+
+function generateBakery(bakeryObj) {
+  let outerDiv = document.getElementById("outerDiv");
+  outerDiv.setAttribute(
+    "class",
+    "row h-25 justify-content-center align-items-center"
+  );
+
+  let bakeryContainer = document.createElement("div");
+  let div = document.createElement("div");
+  let bakeryInfo = document.createElement("div");
+  let bakeryName = document.createElement("h4");
+  let bakeryLocation = document.createElement("h6");
+  let bakeryManager = document.createElement("p");
+  let bakeryDescription = document.createElement("p");
+  let bakeryFooter = document.createElement("div");
+  let editBakeryBtn = document.createElement("button");
+  let deleteBakeryBtn = document.createElement("button");
+
+  div.setAttribute("class", "w-100 card");
+  div.setAttribute("style", "margin-bottom: 12px");
+  bakeryInfo.setAttribute("class", "card-body");
+  bakeryName.setAttribute("class", "card-title");
+  bakeryLocation.setAttribute("class", "card-subtitle mb-2 text-muted");
+  bakeryManager.setAttribute = ("class", "card-text");
+  bakeryFooter.setAttribute("class", "card-footer");
+  editBakeryBtn.setAttribute("class", "btn btn-secondary");
+  editBakeryBtn.setAttribute("style", "margin-right: 8px");
+  deleteBakeryBtn.setAttribute("class", "btn btn-outline-danger");
+
+  bakeryName.innerHTML = bakeryObj.bakeryName;
+  bakeryLocation.innerHTML = bakeryObj.bakeryLocation;
+  bakeryManager.innerHTML = "Managed by " + bakeryObj.bakeryManager;
+  bakeryDescription.innerHTML = bakeryObj.bakeryDescription;
+  editBakeryBtn.innerHTML = "Edit bakery";
+  deleteBakeryBtn.innerHTML = "Delete bakery";
+  editBakeryBtn.setAttribute("onclick", "editBakery()");
+  deleteBakeryBtn.setAttribute("onclick", "deleteBakery()");
+
+  bakeryContainer.appendChild(div);
+  div.appendChild(bakeryInfo);
+  bakeryInfo.appendChild(bakeryName);
+  bakeryInfo.appendChild(bakeryLocation);
+  bakeryInfo.appendChild(bakeryManager);
+  bakeryInfo.appendChild(bakeryDescription);
+  div.appendChild(bakeryFooter);
+  bakeryFooter.appendChild(editBakeryBtn);
+  bakeryFooter.appendChild(deleteBakeryBtn);
+
+  document.getElementById("container").appendChild(div);
+}
+
+function editBakery() {}
+
+function deleteBakery() {}
+
+// ---------------------------------------DESSERTS--------------------------------------------
+function getDesserts() {
+  (async () => {
+    let result = await fetch(endPointRoot + "/dessert")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((res) => {
+        document.getElementById("container").innerHTML = "";
+
+        let dessertTitle = document.createElement("h2");
+        dessertTitle.setAttribute("class", "text-info font-weight-bold");
+        dessertTitle.innerHTML = "Desserts";
+
+        let button = document.createElement("BUTTON");
+        button.innerHTML = "Add new dessert";
+        button.setAttribute("class", "w-100 btn btn-outline-info");
+        button.setAttribute("onclick", "addDessertFields()");
+        button.setAttribute("style", "margin-bottom: 24px");
+        button.setAttribute("data-toggle", "modal");
+        button.setAttribute("data-target", "#addDessertModal");
+
+        document.getElementById("container").appendChild(dessertTitle);
+        document.getElementById("container").appendChild(button);
+        for (let i = 0; i < res.length; i++) {
+          generateDessert(res[i]);
+        }
+      });
+  })();
+}
+
+async function getBakery(value) {
+  let bakeryName = "";
+  const result = await fetch(endPointRoot + "/bakery/?id=" + value)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((res) => {
+      console.log(res);
+      console.log(res[0].bakeryName);
+      bakeryName = res[0].bakeryName;
+      return res[0].bakeryName;
+    });
+  console.log("getBakery bakeryName" + bakeryName);
+  return bakeryName;
+}
+
+function generateDessert(dessertObj) {
+  let outerDiv = document.getElementById("outerDiv");
+  outerDiv.setAttribute(
+    "class",
+    "row h-25 justify-content-center align-items-center"
+  );
+
+  let dessertContainer = document.createElement("div");
+  let div = document.createElement("div");
+
+  let dessertInfo = document.createElement("div");
+  let dessertName = document.createElement("h4");
+  let dessertIngredients = document.createElement("h6");
+  let dessertDescription = document.createElement("p");
+  let dessertBakery = document.createElement("p");
+
+  let dessertFooter = document.createElement("div");
+  let editDessertBtn = document.createElement("button");
+  let deleteDessertBtn = document.createElement("button");
+
+  div.setAttribute("class", "w-100 card");
+  div.setAttribute("style", "margin-bottom: 12px");
+  dessertInfo.setAttribute("class", "card-body");
+  dessertName.setAttribute("class", "card-title");
+  dessertIngredients.setAttribute("class", "card-subtitle mb-2 text-muted");
+  dessertDescription.setAttribute = ("class", "card-text");
+  dessertFooter.setAttribute("class", "card-footer");
+  editDessertBtn.setAttribute("class", "btn btn-secondary");
+  editDessertBtn.setAttribute("style", "margin-right: 8px");
+  deleteDessertBtn.setAttribute("class", "btn btn-outline-danger");
+
+  dessertName.innerHTML = dessertObj.dessertName;
+  dessertIngredients.innerHTML =
+    "Ingredients: " + dessertObj.dessertIngredients;
+  dessertDescription.innerHTML = dessertObj.dessertDescription;
+  dessertBakery.innerHTML =
+    "Available in " + getBakery(dessertObj.bakeryID) + " bakery";
+
+  console.log("Generate dessert " + dessertObj.bakeryID);
+  console.log("Generate dessert " + getBakery(dessertObj.bakeryID));
+
+  editDessertBtn.innerHTML = "Edit dessert";
+  deleteDessertBtn.innerHTML = "Delete dessert";
+  editDessertBtn.setAttribute("onclick", "editDessert()");
+  deleteDessertBtn.setAttribute("onclick", "deleteDessert()");
+
+  dessertContainer.appendChild(div);
+  div.appendChild(dessertInfo);
+
+  dessertInfo.appendChild(dessertName);
+  dessertInfo.appendChild(dessertIngredients);
+  dessertInfo.appendChild(dessertDescription);
+  dessertInfo.appendChild(dessertBakery);
+
+  div.appendChild(dessertFooter);
+  dessertFooter.appendChild(editDessertBtn);
+  dessertFooter.appendChild(deleteDessertBtn);
+
+  document.getElementById("container").appendChild(div);
+}
+
+// ---------------------------------------EMPLOYEES--------------------------------------------
+function getEmployees() {
+  (async () => {
+    let result = await fetch(endPointRoot + "/employee")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((res) => {
+        document.getElementById("container").innerHTML = "";
+
+        let employeeTitle = document.createElement("h2");
+        employeeTitle.setAttribute("class", "text-success font-weight-bold");
+        employeeTitle.innerHTML = "Employees";
+
+        let button = document.createElement("BUTTON");
+        button.innerHTML = "Add new employee";
+        button.setAttribute("class", "w-100 btn btn-outline-success");
+        button.setAttribute("onclick", "addEmployeeFields()");
+        button.setAttribute("style", "margin-bottom: 24px");
+        button.setAttribute("data-toggle", "modal");
+        button.setAttribute("data-target", "#addEmployeeModal");
+
+        document.getElementById("container").appendChild(employeeTitle);
+        document.getElementById("container").appendChild(button);
+        for (let i = 0; i < res.length; i++) {
+          generateEmployee(res[i]);
+        }
+      });
+  })();
+}
+
+function generateEmployee(employeeObj) {
+  let outerDiv = document.getElementById("outerDiv");
+  outerDiv.setAttribute(
+    "class",
+    "row h-25 justify-content-center align-items-center"
+  );
+
+  let employeeContainer = document.createElement("div");
+  let div = document.createElement("div");
+
+  let employeeInfo = document.createElement("div");
+  let employeeName = document.createElement("h4");
+  let employeeRole = document.createElement("h6");
+  let employeeBakery = document.createElement("p");
+  let employeeDescription = document.createElement("p");
+
+  let employeeFooter = document.createElement("div");
+  let editEmployeeBtn = document.createElement("button");
+  let deleteEmployeeBtn = document.createElement("button");
+
+  div.setAttribute("class", "w-100 card");
+  div.setAttribute("style", "margin-bottom: 12px");
+  employeeInfo.setAttribute("class", "card-body");
+  employeeName.setAttribute("class", "card-title");
+  employeeRole.setAttribute("class", "card-subtitle mb-2 text-muted");
+  employeeBakery.setAttribute = ("class", "card-text");
+  employeeDescription.setAttribute = ("class", "card-text");
+
+  employeeFooter.setAttribute("class", "card-footer");
+  editEmployeeBtn.setAttribute("class", "btn btn-secondary");
+  editEmployeeBtn.setAttribute("style", "margin-right: 8px");
+  deleteEmployeeBtn.setAttribute("class", "btn btn-outline-danger");
+
+  employeeName.innerHTML = employeeObj.firstName + " " + employeeObj.lastName;
+  employeeRole.innerHTML = employeeObj.role;
+  employeeBakery.innerHTML =
+    "Works in bakery " + getBakery(employeeObj.bakeryID);
+  employeeDescription.innerHTML = employeeObj.description;
+
+  editEmployeeBtn.innerHTML = "Edit employee";
+  deleteEmployeeBtn.innerHTML = "Delete employee";
+  editEmployeeBtn.setAttribute("onclick", "editEmployee()");
+  deleteEmployeeBtn.setAttribute("onclick", "deleteEmployee()");
+
+  employeeContainer.appendChild(div);
+  div.appendChild(employeeInfo);
+
+  employeeInfo.appendChild(employeeName);
+  employeeInfo.appendChild(employeeRole);
+  employeeInfo.appendChild(employeeBakery);
+  employeeInfo.appendChild(employeeDescription);
+
+  div.appendChild(employeeFooter);
+  employeeFooter.appendChild(editEmployeeBtn);
+  employeeFooter.appendChild(deleteEmployeeBtn);
+
+  document.getElementById("container").appendChild(div);
+}
--- client/js/admin.js
+++ client/js/admin.js
@@ -1,160 +1,161 @@
-const endPointRoot = "https://ashergum.com/Comp4537/termproject/API/V1"
+const endPointRoot = "https://ashergum.com/Comp4537/termproject/API/V1";
 const xhttp = new XMLHttpRequest();
 
 // post request for bakery
-function postBakery(name, location, manager, description){
-    (async() => {
-        let result = fetch(endPointRoot, {
-            method: 'post',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                type: "Bakery",
-                name: name,
-                location: location,
-                manager: manager,
-                description: description
-            })
-        }).then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-        }).then(() => {
-            // this shows visually that the data has been sent to the database
-            let text = document.createElement("H1");
-            text.innerHTML = "sent";
-            let body = document.getElementById("content")
-            body.append(text);
-            const delay = ms => new Promise(res => setTimeout(res, ms));
-            const yourFunction = async () => {
-                await delay(2000);
-                text.style.display = "none";
-                getBakeries();
-                stats();
-            }
-            yourFunction();
-        })
-    })();
+function postBakery(name, location, manager, description) {
+  (async () => {
+    let result = fetch(endPointRoot, {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        type: "Bakery",
+        name: name,
+        location: location,
+        manager: manager,
+        description: description,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then(() => {
+        // this shows visually that the data has been sent to the database
+        let text = document.createElement("H1");
+        text.innerHTML = "sent";
+        let body = document.getElementById("content");
+        body.append(text);
+        const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+        const yourFunction = async () => {
+          await delay(2000);
+          text.style.display = "none";
+          getBakeries();
+          stats();
+        };
+        yourFunction();
+      });
+  })();
 }
 
 function stats() {
-    (async() => {
-        let result = await fetch(endPointRoot + "/stats").then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-        }).then((res) => {
-            document.getElementById("stats").innerHTML = "";
-            for(let i = 0; i < res.length; i++){
-                generateStats(res[i]);
-            }
-        })
-    })();
+  (async () => {
+    let result = await fetch(endPointRoot + "/stats")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((res) => {
+        document.getElementById("stats").innerHTML = "";
+        for (let i = 0; i < res.length; i++) {
+          generateStats(res[i]);
+        }
+      });
+  })();
 }
 
-
-function getBakeries () {
-    (async() => {
-        let result = await fetch(endPointRoot).then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-        }).then((res) => {
-            document.getElementById("container").innerHTML = "";
-            for(let i = 0; i < res.length; i++){
-                generateBakery(res[i]);
-            }
-        })
-    })();
+function getBakeries() {
+  (async () => {
+    let result = await fetch(endPointRoot)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((res) => {
+        document.getElementById("container").innerHTML = "";
+        for (let i = 0; i < res.length; i++) {
+          generateBakery(res[i]);
+        }
+      });
+  })();
 }
 
-function generateBakery(bakeryObj){
-    let aside = document.createElement("aside");
-    //Name of Bakery
-        let info_column1 = document.createElement("div");
-        info_column1.className = "info_column";
-            let label1 = document.createElement("p");
-            label1.innerHTML = "Name of Bakery";
-            label1.className = "label";
-            let item1 = document.createElement("p");
-            item1.className = "item";
-            item1.innerHTML = bakeryObj.bakeryName;
+function generateBakery(bakeryObj) {
+  let aside = document.createElement("aside");
+  //Name of Bakery
+  let info_column1 = document.createElement("div");
+  info_column1.className = "info_column";
+  let label1 = document.createElement("p");
+  label1.innerHTML = "Name of Bakery";
+  label1.className = "label";
+  let item1 = document.createElement("p");
+  item1.className = "item";
+  item1.innerHTML = bakeryObj.bakeryName;
 
-    document.getElementById("container").append(aside);
-    aside.append(info_column1);
-    info_column1.append(label1);
-    info_column1.append(item1);
+  document.getElementById("container").append(aside);
+  aside.append(info_column1);
+  info_column1.append(label1);
+  info_column1.append(item1);
 
-    //Location of Bakery
-        let info_column2 = document.createElement("div");
-        info_column2.className = "info_column";
-            let label2 = document.createElement("p");
-            label2.innerHTML = "Location of Bakery";
-            label2.className = "label";
-            let item2 = document.createElement("p");
-            item2.className = "item";
-            item2.innerHTML = bakeryObj.bakeryLocation;
+  //Location of Bakery
+  let info_column2 = document.createElement("div");
+  info_column2.className = "info_column";
+  let label2 = document.createElement("p");
+  label2.innerHTML = "Location of Bakery";
+  label2.className = "label";
+  let item2 = document.createElement("p");
+  item2.className = "item";
+  item2.innerHTML = bakeryObj.bakeryLocation;
 
-    aside.append(info_column2);
-    info_column2.append(label2);
-    info_column2.append(item2);
+  aside.append(info_column2);
+  info_column2.append(label2);
+  info_column2.append(item2);
 
-    //Manager of Bakery
-        let info_column3 = document.createElement("div");
-        info_column3.className = "info_column";
-            let label3 = document.createElement("p");
-            label3.innerHTML = "Manager of Bakery";
-            label3.className = "label";
-            let item3 = document.createElement("p");
-            item3.className = "item";
-            item3.innerHTML = bakeryObj.bakeryManager;
+  //Manager of Bakery
+  let info_column3 = document.createElement("div");
+  info_column3.className = "info_column";
+  let label3 = document.createElement("p");
+  label3.innerHTML = "Manager of Bakery";
+  label3.className = "label";
+  let item3 = document.createElement("p");
+  item3.className = "item";
+  item3.innerHTML = bakeryObj.bakeryManager;
 
+  aside.append(info_column3);
+  info_column3.append(label3);
+  info_column3.append(item3);
 
-    aside.append(info_column3);
-    info_column3.append(label3);
-    info_column3.append(item3);
+  //Description of Bakery
+  let info_column4 = document.createElement("div");
+  info_column4.className = "info_column";
+  let label4 = document.createElement("p");
+  label4.innerHTML = "Description of Bakery";
+  label4.className = "label";
+  let item4 = document.createElement("p");
+  item4.className = "item";
+  item4.innerHTML = bakeryObj.bakeryDescription;
 
-    //Description of Bakery
-        let info_column4 = document.createElement("div");
-        info_column4.className = "info_column";
-            let label4 = document.createElement("p");
-            label4.innerHTML = "Description of Bakery";
-            label4.className = "label";
-            let item4 = document.createElement("p");
-            item4.className = "item";
-            item4.innerHTML = bakeryObj.bakeryDescription;
-
-    aside.append(info_column4);
-    info_column4.append(label4);
-    info_column4.append(item4);
-        
+  aside.append(info_column4);
+  info_column4.append(label4);
+  info_column4.append(item4);
 }
 
-function generateStats(statsObj){
-    let stats = document.getElementById("stats");
-    console.log(statsObj);
-    let text1 = document.createElement("p");
-    let text2 = document.createElement("p");
-    let text3 = document.createElement("p");
-    let text4 = document.createElement("p");
-    let text5 = document.createElement("p");
-    
-    text1.innerHTML = statsObj.Endpoint;
-    stats.append(text1);
-        
-    text2.innerHTML = statsObj.GET_stat;
-    stats.append(text2);
-        
-    text3.innerHTML = statsObj.POST_stat;
-    stats.append(text3);
-        
-    text4.innerHTML = statsObj.PUT_stat;
-    stats.append(text4);
-        
-    text5.innerHTML = statsObj.DELETE_stat;
-    stats.append(text5);
+function generateStats(statsObj) {
+  let stats = document.getElementById("stats");
+  console.log(statsObj);
+  let text1 = document.createElement("p");
+  let text2 = document.createElement("p");
+  let text3 = document.createElement("p");
+  let text4 = document.createElement("p");
+  let text5 = document.createElement("p");
+
+  text1.innerHTML = statsObj.Endpoint;
+  stats.append(text1);
+
+  text2.innerHTML = statsObj.GET_stat;
+  stats.append(text2);
+
+  text3.innerHTML = statsObj.POST_stat;
+  stats.append(text3);
+
+  text4.innerHTML = statsObj.PUT_stat;
+  stats.append(text4);
+
+  text5.innerHTML = statsObj.DELETE_stat;
+  stats.append(text5);
 }
-
-
 
 getBakeries();
 stats();
